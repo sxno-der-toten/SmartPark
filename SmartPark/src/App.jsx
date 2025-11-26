4a669975345367dd39780af8340d574ee59b205a
sano-der-toten <146814956+sxno-der-toten@users.noreply.github.com>
2025-11-26 09:40:45 +0100

    Initialisation du projet

---
+++ SmartPark/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import logo from './assets/logo.png';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <img className='logo' src={logo} alt="CSS Logo" />
+    </>
+  )
+}
+
+export default App
